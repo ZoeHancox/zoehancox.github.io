4cafe404a142d053e949a700c50ffaa0e0e2a24e
ZoeHancox <83767921+ZoeHancox@users.noreply.github.com>
2024-01-03 09:05:10 +0000

    Streamlit file added

---
+++ ml_streamlit.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Machine Learning Basics")
+
+
+st.markdown("This page provides a brief and basic description of the basics of different machine learning methods.")
+
+with st.expander(rf"""**Linear Regression**"""):
+    st.write("Linear regression is...")
+
+# TODO: could do a page on the different ways to get model results e.g. odds ratio.
